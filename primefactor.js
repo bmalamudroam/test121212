b5c7524ed528707e156b001ff80c80b3ebb1529a
Benjamin Malamud-Roam <bmalamudroam@gmail.com>
2020-11-04 15:56:18 -0800

    implemented populateObjWithPrimeFactors

---
+++ primefactor.js
@@ -0,0 +1,32 @@
+var obj = {};
+var populateObjWithPrimeFactor = function(obj, num) {
+  if (isPrime(num)) {
+    obj[num] = (obj[num] || 0) + 1;
+    return;
+  }
+  var i = 2;
+  while (i <= Math.ceil(Math.sqrt(num))){
+    if (num % i === 0) {
+      num = num / i;
+      populateObjWithPrimeFactor(obj, i);
+      populateObjWithPrimeFactor(obj, num);
+      return;
+    }
+    i++;
+  }
+
+};
+var isPrime = function(num) {
+  if (num < 2) {
+    return false;
+  }
+  if (num === 2) {
+    return true;
+  }
+  for (var i = 2; i <= Math.ceil(Math.sqrt(num)); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
